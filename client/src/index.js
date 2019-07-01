781513acff12c10d765a718672f32bbc36425393
SDK <s.danyal.k@gmail.com>
2019-07-01 11:28:29 -0700

    added travis ci (#13)

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-// eslint-disable-next-line react/jsx-filename-extension
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
